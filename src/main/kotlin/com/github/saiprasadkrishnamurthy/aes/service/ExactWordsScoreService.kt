ef80e84c3b98340a77a01ea75e5b72cf15250a4b
saiprasadkrishnamurthy <saiprasad.krishnamurthy@sita.aero>
2020-07-24 14:52:11 +0530

    Weightages

--- src/main/kotlin/com/github/saiprasadkrishnamurthy/aes/service/ExactWordsScoreService.kt
+++ src/main/kotlin/com/github/saiprasadkrishnamurthy/aes/service/ExactWordsScoreService.kt
@@ -13,17 +13,19 @@ import org.springframework.stereotype.Service
 class ExactWordsScoreService(messagePublisher: MessagePublisher) : BaseScoreService(messagePublisher) {
     override fun getScore(questionAnswerMetadata: QuestionAnswerMetadata): Score {
         return if (questionAnswerMetadata.actualAnswer.isNotBlank()) {
-            // your logic call Score.n(...) with the actual score.
             val actualAnswerIgnoringSpaces = questionAnswerMetadata.actualAnswer.replace("\\s+", "")
             val exactAnswers = questionAnswerMetadata.exactTexts
             var score = 0.0;
             exactAnswers.forEach {
-                val keywordIgnoringSpaces = it.keyword.replace("\\s+","")
+                val keywordIgnoringSpaces = it.keyword.replace("\\s+", "")
                 if (actualAnswerIgnoringSpaces.contains(keywordIgnoringSpaces, true)) {
                     score++;
                 }
             }
-            Score.n(qmId = questionAnswerMetadata.id, answerType = AnswerType.actual, type = "exactWords", n = score)
+            Score.n(qmId = questionAnswerMetadata.id,
+                    answerType = AnswerType.actual,
+                    type = "exactWords",
+                    n = score * questionAnswerMetadata.weightages.getOrDefault("exactWords", 1.0))
         } else {
             Score.one(qmId = questionAnswerMetadata.id, answerType = AnswerType.expected, type = "exactWords")
         }
